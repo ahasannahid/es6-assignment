21d83ffc42b55310b88941ff215e54b889e84eb0
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-08-13 18:09:58 +0600

    array map

---
+++ map.js
@@ -0,0 +1,9 @@
+function doubleNumbers(numbers) {
+    return numbers.map(number => number * 2);
+  }
+  
+  
+  const inputNumbers = [1, 2, 3, 4, 5];
+  const doubledArray = doubleNumbers(inputNumbers);
+  console.log(doubledArray);
+  
